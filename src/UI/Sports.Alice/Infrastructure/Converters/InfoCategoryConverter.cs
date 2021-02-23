f30393afcc0d6d7c1bab04d877d86ec3f1853687
alexvolchetsky <alex.volchetsky@gmail.com>
2021-02-23 14:36:04 +0300

    fix tests

--- src/UI/Sports.Alice/Infrastructure/Converters/InfoCategoryConverter.cs
+++ src/UI/Sports.Alice/Infrastructure/Converters/InfoCategoryConverter.cs
@@ -24,7 +24,7 @@ namespace Sports.Alice.Infrastructure.Converters
 
         public override void Write(Utf8JsonWriter writer, InfoCategory value, JsonSerializerOptions options)
         {
-            throw new NotImplementedException();
+            writer.WriteStringValue(value.ToString().ToLower());
         }
     }
 }
